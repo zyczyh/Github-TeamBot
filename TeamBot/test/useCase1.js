7411c83380e9eb293adddfbbcd9b9fe9636109f7
hwu23 <hwu23@ncsu.edu>
2019-10-23 00:16:37 -0400

    usecase1 done

--- TeamBot/test/useCase1.js
+++ TeamBot/test/useCase1.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer-core');
         "browserWSEndpoint": "ws://localhost:9222/devtools/browser/8fbce3f5-ee42-4c75-9621-1a7f56b70f82"
     });
     const page = await browser.newPage();
-    await page.goto("http://192.168.6.130:8065/csc510/channels/team19");
+    await page.goto("http://192.168.6.130:8065/csc510/channels/town-square");
 
     const textSelector = "#post_textbox";
     await page.waitForSelector(textSelector);
@@ -27,9 +27,9 @@ const puppeteer = require('puppeteer-core');
     // });
 
     // for test, delete later
-    await page.waitForSelector(textSelector);
-    await page.type(textSelector, "http://192.168.6.130:3000/authen");
-    await page.keyboard.press("Enter");
+    // await page.waitForSelector(textSelector);
+    // await page.type(textSelector, "http://192.168.6.130:3000/authen");
+    // await page.keyboard.press("Enter");
     
     await page.waitForSelector('.post-message__text-container');
     const msg = await page.$$('.post-message__text-container');
@@ -41,14 +41,33 @@ const puppeteer = require('puppeteer-core');
 
     // console.log(url[0]);
 
+    await page.waitFor(2000);
     const authen = await browser.newPage();
     await authen.goto(url[0]);
     await authen.type('body > form > div:nth-child(1) > input[type=text]', '451725e0bb5b36564c7bc33b6c0asdf8394327fb952911bf381ea25ae1174cdae1');
     await authen.type('body > form > div:nth-child(2) > input[type=text]', 'csc510');
+    await authen.waitFor(2000);
     await authen.click('body > form > div.actions > input[type=submit]');
+    await authen.waitFor(2000);
+    await authen.close();
 
-
-    
+    // failed
+    await page.click('#sidebarItem_town-square');
+    await page.waitForSelector(textSelector);
+    await page.type(textSelector, "@teambot @fakeUser");
+    await page.keyboard.press("Enter"); 
+    await page.waitFor(2000);
+    await page.click("#sidebarItem_cti4a6q8kfrr5cfe6adtjimxao__wzrhj8qy7tnrxgfuc1k3be64sc");
+    await page.waitFor(3000);
+      
+    // success
+    await page.click('#sidebarItem_town-square');
+    await page.waitForSelector(textSelector);
+    await page.type(textSelector, "@teambot @yanchen");
+    await page.keyboard.press("Enter"); 
+    await page.waitFor(2000);
+    await page.click("#sidebarItem_cti4a6q8kfrr5cfe6adtjimxao__wzrhj8qy7tnrxgfuc1k3be64sc");
+    await page.waitFor(3000);
 
     // await browser.close();
   })();
