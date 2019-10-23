5925001edaffb5198f675382e9dcbdc3b410b93a
xliu16 <32623183+xliu16@users.noreply.github.com>
2019-10-22 22:15:00 -0400

    test

---
+++ TeamBot/seleniumTest/test_mission_trigger.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.connect({
+        "browserWSEndpoint": "ws://localhost:9222/devtools/browser/fd401619-1ab9-45d3-94b0-815bc21d0d16"
+    });
+    const page = await browser.newPage();
+    
+    await page.goto('http://localhost:3000/test/mission-trigger');
+    await page.click("body > form > div > input[type=submit]");
+    await page.close({runBeforeUnload:true});
+
+    const mattermost_page = await browser.newPage();
+    await mattermost_page.goto('https://csc510-mattermost-19.herokuapp.com');
+    await mattermost_page.waitFor(5000);
+    await mattermost_page.screenshot({path: 'mission_trigger.png'});
+    // await browser.close();
+})().catch(function(e) {
+    console.log(e);
+});
