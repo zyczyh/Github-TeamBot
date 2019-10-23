b82c19ed67195dc1bad77d04dd1a4615a024a5f8
xliu16 <32623183+xliu16@users.noreply.github.com>
2019-10-22 22:10:17 -0400

    selenium test added

--- TeamBot/routes/test.js
+++ TeamBot/routes/test.js
@@ -5,16 +5,26 @@ var mission = require('../Controller/missionController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var data = report.analysis();
+    // var data = report.analysis();
     res.render('test', { data: data});
 });
 
 router.get('/mission-trigger', function (req, res, next) {
     res.render('test/mission-trigger');
 });
-
-router.post('/mission-trigger', function (req, res, next) {
+// function logOut() {
+//     $.post("/logout").then(function(data) {
+//         window.location = data.redirectUrl;
+//     });
+// };
+router.post('/logout', function (req, res, next) {
     mission.weeklyReport();
+    // res.logOut();
+    // res.logout();
+    // req.session.destroy();
+    // res.send({err: 0, redirectUrl: "test/mission-trigger"});
+    // res.render('test/mission-trigger');
+    res.redirect(307, 'test/mission-trigger');
 });
 
 module.exports = router;
